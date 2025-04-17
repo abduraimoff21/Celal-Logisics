d7d4854d7aef705c21fba005eee5350d039078a4
abduraimoff21 <abduraimov21doston@gmail.com>
2025-04-17 14:03:37 +0500

    updated server.js based on updates of Node.js Driver

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
