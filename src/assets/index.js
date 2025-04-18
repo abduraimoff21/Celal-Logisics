d850ef92e2a3859433585ae8d63e21cbafc256f1
abduraimoff21 <abduraimov21doston@gmail.com>
2025-04-18 23:48:24 +0500

    modified:   src/assets/index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,21 +1,21 @@
-import logo from "/assets/logo2.svg";
-import mainlogo from "/assets/favicon.ico";
-import yourlogo from "/assets/yourlogo.svg";
+import logo from "/images/logo2.svg";
+import mainlogo from "/images/favicon.ico";
+import yourlogo from "/images/yourlogo.svg";
 
 import curve from "./hero/curve.png";
 
-import benefitCard1 from "/assets/card-1.svg";
-import benefitCard2 from "/assets/card-2.svg";
-import benefitCard3 from "/assets/card-3.svg";
-import benefitCard4 from "/assets/card-4.svg";
-import benefitCard5 from "/assets/card-5.svg";
-import benefitCard6 from "/assets/card-6.svg";
+import benefitCard1 from "/images/card-1.svg";
+import benefitCard2 from "/images/card-2.svg";
+import benefitCard3 from "/images/card-3.svg";
+import benefitCard4 from "/images/card-4.svg";
+import benefitCard5 from "/images/card-5.svg";
+import benefitCard6 from "/images/card-6.svg";
 
-import whatsapp from "/assets/whatsapp.svg";
-import facebook from "/assets/facebook.svg";
-import instagram from "/assets/instagram.svg";
-import telegram from "/assets/telegram.svg";
-import email from "/assets/email.svg";
+import whatsapp from "/images/whatsapp.svg";
+import facebook from "/images/facebook.svg";
+import instagram from "/images/instagram.svg";
+import telegram from "/images/telegram.svg";
+import email from "/images/email.svg";
 
 export {
   logo,
